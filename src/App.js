41190b9bf944f438eb93c39d205501975ca4f7d1
Saugat Acharya <mesaugat@gmail.com>
2017-12-29 15:07:54 +0545

    Initial todo list html

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
 
 /**
- * A simple function which returns some text inside a <h1> tag.
+ * Return HTML skeleton for todo list.
  */
 function App() {
   return (
-    <h1>Let's Learn React</h1>
-  )
+    <div className="container">
+      <div className="row">
+        <div className="todolist">
+          <h1>Todos</h1>
+          <ul className="list-unstyled">
+            <li className="ui-state-default">
+              <div className="checkbox">
+                  <label>
+                    <input type="checkbox" value=""/>Take out the trash
+                  </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+              <div className="checkbox">
+                  <label>
+                    <input type="checkbox" value=""/>Buy bread
+                  </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value=""/>Teach penguins to fly
+                </label>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+// Add boostrap css
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Add our style
+import './assets/style/index.css';
+
 // Render a React element to the DOM
 ReactDOM.render(<App />, document.getElementById('root'));
