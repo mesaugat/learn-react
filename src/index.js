2747a6ba6688c515ad69673bc6831031737b2b80
Saugat Acharya <mesaugat@gmail.com>
2017-12-29 14:34:27 +0545

    Hello world of React

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
+import App from './App';
+
+// Render a React element to the DOM
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
