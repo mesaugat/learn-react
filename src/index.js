bb9d47d4d833c90acd7caf83397fdf040e8ab2d5
Saugat Acharya <mesaugat@gmail.com>
2017-12-29 20:25:29 +0545

    Move files to components directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './components/App';
 
 // Add boostrap css
 import 'bootstrap/dist/css/bootstrap.css';
