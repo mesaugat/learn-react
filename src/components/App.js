7fa44b01288b0712ab765f427cddefeefe4c2c14
Saugat Acharya <mesaugat@gmail.com>
2017-12-29 20:56:32 +0545

    Make App component stateful

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import TodoList from './TodoList';
 
 /**
  * App component.
  */
-const App = () => {
-  const items = [
-    {
-      id: 1,
-      text: 'Take out the trash',
-      completed: false
-    },
-    {
-      id: 2,
-      text: 'Buy bread',
-      completed: false
-    },
-    {
-      id: 3,
-      text: 'Teach penguins to fly',
-      completed: true
-    }
-  ];
+class App extends Component {
+   state = {
+    items: [
+      {
+        id: 1,
+        text: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        text: 'Buy bread',
+        completed: false
+      },
+      {
+        id: 3,
+        text: 'Teach penguins to fly',
+        completed: true
+      }
+    ]
+  };
 
-  return (
-    <div className="container">
-      <div className="row">
-        <TodoList items={items} />
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <TodoList items={this.state.items} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
